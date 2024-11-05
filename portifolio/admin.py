84aa9990db83bd035810b8038770339c2704662d
Heder Lopes <sistemas.heder@gmail.com>
2024-11-05 18:20:38 -0300

    ajustes nas migrations

--- portifolio/admin.py
+++ portifolio/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Personal, Contact, Address, Complementary_Courses, Education, Experience, Interests, Skills, Workflow
+
+class PersonalAdmin(admin.ModelAdmin):
+    ...
+
+class AddressAdmin(admin.ModelAdmin):
+    ...
+
+class ContactAdmin(admin.ModelAdmin):
+    ...
+
+class Complementary_CoursesAdmin(admin.ModelAdmin):
+    ...
+
+class EducationAdmin(admin.ModelAdmin):
+    ...
+
+class ExperienceAdmin(admin.ModelAdmin):
+    ...
+
+class InterestsAdmin(admin.ModelAdmin):
+    ...
+
+class SkillsAdmin(admin.ModelAdmin):
+    ...
+
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Personal, PersonalAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Complementary_Courses, Complementary_CoursesAdmin)
+admin.site.register(Education,EducationAdmin)
+admin.site.register(Experience, ExperienceAdmin)
+admin.site.register(Interests, InterestsAdmin)
+admin.site.register(Skills, SkillsAdmin)
+admin.site.register(Workflow, WorkflowAdmin)
+
--- portifolio/models.py
+++ portifolio/models.py
@@ -14,6 +14,9 @@ class Personal(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
 
+    def __str__(self):
+        return self.first_name
+
 
 class Address(models.Model):
     street = models.CharField(max_length=45)
@@ -25,10 +28,13 @@ class Address(models.Model):
     complement = models.CharField(max_length=45)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.street
 
 
 class Contact(models.Model):
@@ -36,11 +42,14 @@ class Contact(models.Model):
     descrption_contact = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
 
+    def __str__(self):
+        return self.descrption_contact
+    
 
 class Complementary_Courses(models.Model):
     course = models.CharField(max_length=100)
@@ -51,57 +60,83 @@ class Complementary_Courses(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.course
 
 
 class Education(models.Model):
     university = models.CharField(max_length=45)
     academic_title = models.CharField(max_length=45)
     course = models.CharField(max_length=45)
+    final_project_theme = models.CharField(max_length=200, null=True)
+    file = models.FileField(upload_to='portifolio/covers/%Y/%m/%d/', null=True)
     initialized_at = models.DateField(auto_now=False)
     finished_at = models.DateField(auto_now=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
 
+    def __str__(self):
+        return self.academic_title
+    
 
 class Experience(models.Model):
     company = models.CharField(max_length=45)
     positions = models.CharField(max_length=45)
     initialized_at = models.DateField(auto_now=False)
-    finished_at = models.DateField(auto_now=False)
+    finished_at = models.DateField(null=True, blank=True)
     reponsabilities = models.TextField()
     visible = models.BooleanField(True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
+    def __str__(self):
+        return self.company
 
 
 class Interests(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
-
+    def __str__(self):
+        return self.description
 
 class Skills(models.Model):
     skill_name = models.CharField(max_length=45)
-    icon = models.CharField(max_length=45)
+    visible_icon = models.BooleanField(True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=False)
+    deleted_at = models.DateTimeField(auto_now=True)
     personal_id = models.ForeignKey(
         Personal, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.skill_name
+    
+class Workflow(models.Model):
+    workflow_name = models.CharField(max_length=250)    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+    personal_id = models.ForeignKey(
+        Personal, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.workflow_name
