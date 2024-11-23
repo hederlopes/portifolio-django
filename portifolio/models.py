ed3c3987f3c45a8db8fade6d134be87d000d2dc8
Heder Lopes <sistemas.heder@gmail.com>
2024-11-23 00:22:46 -0300

    criação da tabela Message

--- portifolio/models.py
+++ portifolio/models.py
@@ -144,3 +144,15 @@ class Workflow(models.Model):
 
     def __str__(self):
         return self.workflow_name
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
